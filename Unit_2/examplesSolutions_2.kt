14df6581adc2bfae799de8e79b1ecb8673577260
telepinu <telepenu@gmail.com>
2022-05-01 21:32:27 +0300

    Create examplesSolutions_2.kt
    adding first branch and first files

---
+++ Unit_2/examplesSolutions_2.kt
@@ -0,0 +1,6 @@
+/*2.24 (Largest and Smallest Integers) Write an application that reads five integers and determines
+and prints the largest and smallest integers in the group. Use only the programming techniques you
+learned in this chapter.*/
+
+fun main{}
+
